d1532988c5b852142b93785c1a9b84330b7ad479
Benji <benji@benjisoft.org.uk>
2017-10-16 14:46:05 +0100

    Added Creep.py

---
+++ Creep.py
@@ -0,0 +1,4 @@
+name = input("Hello there, what's your name, CHILD? \n")
+age1 = int(input("And how old are you "+ name+ "\n"))
+age2 = int(input("And your father?"+ "\n"))
+print(name, "you are", age2-age1, "years younger. ")
