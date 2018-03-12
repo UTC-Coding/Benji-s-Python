0f4f73516c25c552f0f77cb9937a39b55157d599
Benji <benji@benjicraft.info>
2018-03-12 12:11:05 +0000

    Uploaded from the Mac

---
+++ Times tables (Mac).py
@@ -0,0 +1,6 @@
+number = input("Please enter the multiple you would like... \n")
+quantity = input("Please enter how many multiple you would like... \n ")
+quantity = quantity+1
+
+for count in range(quantity):
+    print(number*count)
