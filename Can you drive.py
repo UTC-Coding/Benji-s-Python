b50e0d00dbeb710246ecbd95f7e83836b161875d
Benji <benji@benjicraft.info>
2018-03-12 12:26:38 +0000

    Completed flowchart activity

---
+++ Can you drive.py
@@ -0,0 +1,8 @@
+age = input("Enter your age ")
+
+if age > 17:
+    print("You can drive a car")
+elif age > 15:
+    print("You could get a motorbike!")
+else:
+    print("You need to keep walking")
